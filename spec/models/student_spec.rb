2257cbb632382bcc8e78b46dc0add60be2e436c0
Karl Macleod <karl.macleod@readytech.io>
2021-04-01 16:37:13 +1100

    Restructure Student spec in response to feedback

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,15 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe Student, :type => :model do
-    it 'returns title; first, middle, and last names when requesting full_name' do
-        student = create(:student)
-        expect(student.full_name).to eq("#{student.title} #{student.first_name} #{student.middle_name} #{student.last_name}")
-    end
+RSpec.describe Student, type: :model do
+    describe '#full_name' do
+        subject { student.full_name }
+        context 'all fields contain values' do
+            let(:student) { create(:student, title: "Mr", first_name: "Henry", middle_name: "Johnathon", last_name: "Ford", birth_date: "19010710", gender: "M") }
+            
+            it 'returns the full name' do
+                expect(subject).to eq("Mr Henry Johnathon Ford")
+            end
+        end
 
-    it 'returns a single name correctly when requesting full_name, without unwanted spaces' do
-        student = create(:student)
-        student.first_name = nil
-        student.middle_name = nil
-        expect(student.full_name).to eq("#{student.title} #{student.last_name}")
+        context 'first_name and middle_name are missing' do
+            let(:student) { create(:student, title: "Mr", first_name: nil, middle_name: nil, last_name: "Ford", birth_date: "19010710", gender: "M") }
+            
+            it 'returns the full name, without unexpected spaces' do
+                expect(subject).to eq("Mr Ford")
+            end
+        end
     end
 end
